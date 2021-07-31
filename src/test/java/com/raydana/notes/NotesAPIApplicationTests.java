f622e3c155f93c640dd99d81b50a5e6a625a9e7d
raydana <88098407+raydana@users.noreply.github.com>
2021-07-31 10:45:59 +0430

    Add files via upload

--- src/test/java/com/raydana/notes/NotesAPIApplicationTests.java
+++ src/test/java/com/raydana/notes/NotesAPIApplicationTests.java
@@ -1,1 +1,16 @@
+package com.raydana.notes;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NotesAPIApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
