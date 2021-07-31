c400fdc3a76b9a806fa30e5e68b869c81dd5dab3
raydana <88098407+raydana@users.noreply.github.com>
2021-07-31 15:29:18 +0430

    Add files via upload

--- src/main/java/com/raydana/notes/controller/IndexController.java
+++ src/main/java/com/raydana/notes/controller/IndexController.java
@@ -1,4 +1,3 @@
-
 package com.raydana.notes.controller;
 
 
@@ -12,6 +11,6 @@ public class IndexController {
 
     @GetMapping
     public String sayHello() {
-        return "Hello and Welcome to the EasyNotes application. You can create a new Note by making a POST request to /api/notes endpoint.";
+        return "Hello and Welcome to the notes_api application. You can create a new Note by making a POST request to /api/notes endpoint.";
     }
 }
--- src/main/java/com/raydana/notes/controller/NoteController.java
+++ src/main/java/com/raydana/notes/controller/NoteController.java
@@ -1,51 +1,63 @@
 package com.raydana.notes.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import com.raydana.notes.exception.ResourceNotFoundException;
-import com.raydana.notes.model.Note;
-import com.raydana.notes.repository.NoteRepository;
+import java.util.List;
 
 import javax.validation.Valid;
-import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.raydana.notes.model.Note;
+import com.raydana.notes.service.NoteService;
 
 @RestController
 @RequestMapping("/api")
 public class NoteController {
 
     @Autowired
-    NoteRepository noteRepository;
+    private NoteService noteService;
 
     @GetMapping("/notes")
-    public List<Note> getAllNotes() {
-        return noteRepository.findAll();
+    public List<Note> getAllNotesByUsername(@RequestHeader("header-username") String username) {
+        return noteService.getAllByUsername(username);
     }
+ 
 
     @PostMapping("/notes")
     public Note createNote(@Valid @RequestBody Note note) {
-        return noteRepository.save(note);
+        return noteService.save(note);
     }
 
     @GetMapping("/notes/{id}")
-    public Note getNoteById(@PathVariable(value = "id") Long noteId) {
-        return noteRepository.findById(noteId).orElseThrow(() -> new ResourceNotFoundException("Note", "id", noteId));
+    public Note getNoteById(@PathVariable(value = "id") Long noteId , @RequestHeader("header-username") String username) {
+        return noteService.findById(noteId);
     }
 
     @PutMapping("/notes/{id}")
-    public Note updateNote(@PathVariable(value = "id") Long noteId,@Valid @RequestBody Note noteDetails) {
-        Note note = noteRepository.findById(noteId) .orElseThrow(() -> new ResourceNotFoundException("Note", "id", noteId));
-        note.setTitle(noteDetails.getTitle());
-        note.setContent(noteDetails.getContent());
-        Note updatedNote = noteRepository.save(note);
-        return updatedNote;
+    public String updateNote(@PathVariable(value = "id") Long noteId,@Valid @RequestBody Note noteDetails,@RequestHeader("header-username") String username) {
+    	Boolean hasPermission  = noteService.userHasPermission(noteId, username);
+    	if (hasPermission) {
+    		Note updatedNote = noteService.update(noteId, noteDetails);
+            return "note successfully updated";
+    	}
+    	return "you have not permission for update note Id : "+noteId;
     }
 
     @DeleteMapping("/notes/{id}")
-    public ResponseEntity<?> deleteNote(@PathVariable(value = "id") Long noteId) {
-        Note note = noteRepository.findById(noteId).orElseThrow(() -> new ResourceNotFoundException("Note", "id", noteId));
-        noteRepository.delete(note);
-        return ResponseEntity.ok().build();
+    public String deleteNote(@PathVariable(value = "id") Long noteId,@RequestHeader("header-username") String username) {
+    	Boolean hasPermission  = noteService.userHasPermission(noteId, username);
+    	if (hasPermission) {
+    		noteService.delete(noteId);
+    		return "delete successfully";
+    	}
+    	return "you have not permission for delete note Id : "+noteId;
     }
 }
