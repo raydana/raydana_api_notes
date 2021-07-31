459ed198aba343cf6120b07fcc50131c3c31c2f6
raydana <88098407+raydana@users.noreply.github.com>
2021-07-31 15:31:46 +0430

    Update NoteService.java

--- src/main/java/com/raydana/notes/service/NoteService.java
+++ src/main/java/com/raydana/notes/service/NoteService.java
@@ -1,1 +1,18 @@
 
+package com.raydana.notes.service;
+
+import java.util.List;
+
+import com.raydana.notes.model.Note;
+
+public interface NoteService {
+	public List<Note> getAllByUsername(String username);
+	public List<Note> getAll();
+	public Note save(Note note);
+	public Note findById(Long id);
+	public Note update(Long id,Note noteDetails);
+	public void delete(Long id);
+	public Boolean userHasPermission(Long id , String username);
+	
+
+}
