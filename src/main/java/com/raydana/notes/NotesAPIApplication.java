ddbda727eecc86adab2cd444a4b2171891e25fc0
raydana <88098407+raydana@users.noreply.github.com>
2021-07-31 10:55:19 +0430

    Add files via upload

---
+++ src/main/java/com/raydana/notes/NotesAPIApplication.java
@@ -0,0 +1,14 @@
+package com.raydana.notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class NotesAPIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesAPIApplication.class, args);
+	}
+}
