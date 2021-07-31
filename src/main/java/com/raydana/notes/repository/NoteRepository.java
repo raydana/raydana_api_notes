0f0276ca403b1e1ac6fda3694b0affdd0f35eeff
raydana <88098407+raydana@users.noreply.github.com>
2021-07-31 10:54:56 +0430

    Update NoteRepository.java

--- src/main/java/com/raydana/notes/repository/NoteRepository.java
+++ src/main/java/com/raydana/notes/repository/NoteRepository.java
@@ -1,1 +1,12 @@
 
+package com.raydana.notes.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.raydana.notes.model.Note;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+
+}
