a86573300327b523ea09c6dd0a68fcaf15e07264
raydana <88098407+raydana@users.noreply.github.com>
2021-07-31 15:30:47 +0430

    Add files via upload

--- src/main/java/com/raydana/notes/repository/NoteRepository.java
+++ src/main/java/com/raydana/notes/repository/NoteRepository.java
@@ -1,12 +1,16 @@
-
 package com.raydana.notes.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.raydana.notes.model.Note;
 
 @Repository
 public interface NoteRepository extends JpaRepository<Note, Long> {
-
+	@Query("SELECT n FROM Note n WHERE LOWER(n.username) = LOWER(:username)")
+	public List<Note> getAllByUsername(@Param("username") String username);
 }
